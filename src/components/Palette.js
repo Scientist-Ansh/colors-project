a932fdfb7f94c06c7fc700136e4f70782e85fda0
Ansh <ansh1999arora@gmail.com>
2020-06-09 14:43:14 +0530

    added palette file

---
+++ src/components/Palette.js
@@ -0,0 +1,14 @@
+import  React,{Component} from "react";
+
+export default class Palette extends Component{
+    render(){
+        return(
+            <div className="Palette">
+                {/* Navbar here */}
+                
+                <div className="Palette-colors"></div>
+                {/* footer here */}
+            </div>
+        )
+    }
+}
