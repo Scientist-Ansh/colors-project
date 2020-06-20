97efe033d80a2408e40cc309f28ec1b36cbbc0f0
Ansh <ansh1999arora@gmail.com>
2020-06-20 13:37:28 +0530

    add material ui and select in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,62 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import Slider from '@material-ui/core/Slider';
+
+
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import './Navbar.css';
 
 
-export default class Navbar extends Component{
-    render(){
-        const {level,levelChange} = this.props
-        return(
+export default class Navbar extends Component {
+    constructor(props){
+        super(props);
+            this.state={
+                format:'hex',
+                value:this.props.level
+            }
+            this.handleChange = this.handleChange.bind(this);
+            this.handleSliderChange = this.handleSliderChange.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({format:e.target.value},()=>{
+            this.props.formatChange(this.state.format);
+        })
+    }
+    handleSliderChange(e,value){
+        this.setState({value});
+    }
+
+    render() {
+        const { levelChange } = this.props
+        return (
             <header className="Navbar">
                 <div className="logo">
                     colorPicker
                 </div>
                 <div className="slider-container">
-                    <span>Level: {level}</span>
+                    <span>Level: {this.state.value}</span>
                     <div className="slider">
                         <Slider
-                        defaultValue={level} 
-                        min={100} max={900} 
-                        step={100}
-                        onAfterChange={levelChange}/>
+                            value={this.state.value}
+                            track={false}
+                            min={100} max={900}
+                            step={100}
+                            aria-labelledby="track-false-slider"
+                            onChange={this.handleSliderChange}
+                            onChangeCommitted = {levelChange}/>
                     </div>
                 </div>
-                
+                <div className="select-container">
+                    <Select value = {this.state.format} onChange={this.handleChange}>
+                        <MenuItem value='hex'>Hex</MenuItem>
+                        <MenuItem value='rgb'>Rgb</MenuItem>
+                        <MenuItem value='rgba'>Rgba</MenuItem>
+                    </Select>
+                </div>
+
 
             </header>
         );
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -7,21 +7,26 @@ import './Palette.css';
 
 export default class Palette extends Component{
     state = {
-        level:500
+        level:500,
+        format:"hex"
     }
 
-    handleLevelChange = (level)=>{
+    handleLevelChange = (e,level)=>{
         this.setState({level});
+    }
+
+    handleFormatChange=(format)=>{
+        this.setState({format})
     }
 
     render(){
         console.log(this.state.level)
-        let {level} = this.state;
+        let {level,format} = this.state;
         const colorBoxes = this.props.palette.colors[level].map(color=>
-            <ColorBox background={color.hex} name={color.name} />)
+            <ColorBox background={color[format]} name={color.name} />)
         return(
             <div className="Palette">
-                <NavBar level={level} levelChange={this.handleLevelChange}/>    
+                <NavBar level={level} levelChange={this.handleLevelChange} formatChange={this.handleFormatChange}/>    
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
