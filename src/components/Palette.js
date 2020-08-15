66d901bd9bd0a08721a6ace260da1117a1af574d
Ansh <ansh1999arora@gmail.com>
2020-08-15 16:55:54 +0530

    styles refractor

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,41 +1,42 @@
-import  React,{Component} from "react";
+import React, { Component } from "react";
 
 import ColorBox from './ColorBox';
 import NavBar from './Navbar';
 
 import './Palette.css';
 
-export default class Palette extends Component{
+export default class Palette extends Component {
     state = {
-        level:500,
-        format:"hex"
+        level: 500,
+        format: "hex"
     }
 
-    handleLevelChange = (e,level)=>{
-        this.setState({level});
+    handleLevelChange = (e, level) => {
+        this.setState({ level });
     }
 
-    handleFormatChange=(format)=>{
-        this.setState({format})
+    handleFormatChange = (format) => {
+        this.setState({ format })
     }
 
-    render(){
-        let {level,format} = this.state;
-        const colorBoxes = this.props.palette.colors[level].map(color=>
+    render() {
+        let { level, format } = this.state;
+        const colorBoxes = this.props.palette.colors[level].map(color =>
             <ColorBox background={color[format]}
-             name={color.name} 
-             key={color.id}
-             showAllColors={`/palette/${this.props.palette.id}/${color.id}`}
-             />)
-        return(
+                name={color.name}
+                key={color.id}
+                showAllColors={`/palette/${this.props.palette.id}/${color.id}`}
+            />)
+
+        return (
             <div className="Palette">
                 <NavBar level={level} levelChange={this.handleLevelChange}
-                 formatChange={this.handleFormatChange} allColors/>    
+                    formatChange={this.handleFormatChange} allColors />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
                 <footer className="Palette-footer">
-                    {this.props.palette.paletteName} 
+                    {this.props.palette.paletteName}
                     <span className="Palette-emoji">{this.props.palette.emoji}</span>
                 </footer>
             </div>
